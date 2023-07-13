5e14719ae4d725ccad3b8d0bf3fdb36b21d4d623
obaraelijah <elijahobara357@gmail.com>
2023-07-13 05:57:00 +0000

    Hot fix

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,11 @@
--- Creates a table with unique users.
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- Create table users if it doesn't already exist
+CREATE TABLE IF NOT EXISTS users (
+    -- Unique identifier for each user
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    
+    -- Email of the user (maximum 255 characters)
     email VARCHAR(255) NOT NULL UNIQUE,
+    
+    -- Name of the user (maximum 255 characters)
     name VARCHAR(255)
 );
